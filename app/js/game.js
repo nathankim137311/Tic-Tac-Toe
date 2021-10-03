6b2fd00eb5300c7a57ad2dac0d7db733a2fc16a1
web-dev <nathankim137311@gmail.com>
2021-10-02 19:04:23 -0700

    added reset and start buttons, and put all the games functions into one function startGamer

--- app/js/game.js
+++ app/js/game.js
@@ -1,10 +1,26 @@
 import gameFlow from './flow.js'
 import gameBoard from './board.js'
 
-// creates gameboard
-gameBoard.createBoard();
-gameBoard.clickSquares();
-gameFlow.whoGoesFirst(); 
-gameFlow.checkForWinner(); 
-console.log(gameBoard.grid);
-// creates the players 
+const startBtn = document.createElement('button'); 
+const resetBtn = document.createElement('button'); 
+const btnContainer = document.getElementById('buttons'); 
+btnContainer.appendChild(startBtn); 
+btnContainer.appendChild(resetBtn); 
+startBtn.innerHTML = 'start'; 
+resetBtn.innerHTML = 'reset'; 
+
+resetBtn.addEventListener('click', () => {
+    window.location.reload();
+});
+
+startBtn.addEventListener('click', () => {
+    startGame();
+});
+
+function startGame () {
+    // creates gameboard
+    gameBoard.createBoard();
+    gameBoard.clickSquares();
+    gameFlow.whoGoesFirst(); 
+    gameFlow.checkForWinner();
+}
